942f58c3c878bfacce164884e70895b0242791e7
rvy <rvy@cs.washington.edu>
2019-04-16 00:33:48 -0700

    fix make file

--- app/jni/Android.mk
+++ app/jni/Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := sqlcipher
-LOCAL_SRC_FILES := sqlite3.c encryption.cpp
+LOCAL_SRC_FILES := sqlcipher.cpp
 include $(BUILD_SHARED_LIBRARY)
